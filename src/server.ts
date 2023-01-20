1463725667eaf88606076715a392c267eb8d1eba
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2023-01-20 23:19:12 +0530

    server working fine

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hey there! 👋');
+import { createServer } from "node:http";
+import { createSchema, createYoga } from "graphql-yoga";
+import { schema } from "./schema";
+
+const yoga = createYoga({ schema });
+
+const port = Number(process.env.API_PORT) || 4000;
+const server = createServer(yoga);
+
+server.listen(port, () => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
